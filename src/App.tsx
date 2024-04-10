af3749b650433818e20e804abbe0669cf756b8d9
Aadi <adithyamanikumar@proton.me>
2024-04-10 23:24:14 +0530

    Aah. Now I realize the value of typescript. I could not finish building because typescript detected an error. Nice. Contrary to what I thought initially, I might end up liking typescript after all.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import LoginScreen from "./components/loginscreen/loginscreen";
-import Dashboard from "./components/user-dashboard/dashboard";
 
 function App() {
   return (
     <div>
       <LoginScreen />
-      {/* <Dashboard /> */}
     </div>
   );
 }
