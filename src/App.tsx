df3360d193e08f1451d93bc31387f68c0430d0b5
Aadi <adithyamanikumar@proton.me>
2024-04-11 23:15:54 +0530

    Removed ununsed line in app.tsx (BrowserRouter, it was used in main.tsx and not in app.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import LoginScreen from "./components/loginscreen/loginscreen";
-import { BrowserRouter } from "react-router-dom";
+
 function App() {
   return (
     <div>
