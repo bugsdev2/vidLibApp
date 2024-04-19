7737f335fcf66f879a2c66691e99b4ceb1bbd733
Aadi <adithyamanikumar@proton.me>
2024-04-20 00:45:24 +0530

    Added yup validation

--- src/components/loginform/loginform.tsx
+++ src/components/loginform/loginform.tsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export default function LoginForm() {
+  function handleSubmit(values: object) {
+    console.log(values);
+  }
   return (
     <>
       <div className="text-light text-center h-[88dvh]">
@@ -15,46 +20,77 @@ export default function LoginForm() {
           className="flex justify-center items-start mt-5 bg-[url('/banner5.png')] md:bg-[url('/banner4.png')] h-full"
         >
           <div id="shader"></div>
-          <form
-            id="content"
-            action=""
-            className="rounded-md flex mt-36 flex-col gap-3"
+          <Formik
+            initialValues={{
+              username: "",
+              password: "",
+            }}
+            onSubmit={(values) => {
+              handleSubmit(values);
+            }}
+            validationSchema={Yup.object({
+              username: Yup.string()
+                .required("Please enter a user name")
+                .matches(/^[a-z]\D+/, "Please start with a lowercase letter")
+                .min(6, "User name should be more than 6 letters")
+                .max(15, "User name should be less than 15 letters"),
+              password: Yup.string()
+                .required("Please enter a password")
+                .min(6, "Password should be more than 6 letters")
+                .max(20, "Password should be less than 20 letters")
+                .matches(
+                  /(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])\w{6,20}/,
+                  "Password should have at least one capital letter, one small letter and one number"
+                ),
+            })}
           >
-            <div id="shader"></div>
-            <div className="text-lightred">Login to your Account</div>
+            <Form
+              id="content"
+              action=""
+              className="rounded-md flex mt-36 flex-col gap-3"
+            >
+              <div id="shader"></div>
+              <div className="text-lightred">Login to your Account</div>
 
-            <div className="flex flex-col md:flex-row gap-2 justify-between">
-              <label htmlFor="username">User Name</label>
-              <input
-                id="username"
-                name="username"
-                type="text"
-                className="bg-dark border rounded-lg px-2"
-              />
-            </div>
-            <div className="flex flex-col md:flex-row gap-2 justify-between">
-              <label htmlFor="password">Password</label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                className="bg-dark border rounded-lg px-2"
-              />
-            </div>
-            <div>
-              <button type="button" className="btn mt-3">
-                Login
-              </button>
-              <div>
-                <small>
-                  <Link to="/signup" className="text-primary">
-                    Click here
-                  </Link>{" "}
-                  to create a new account
+              <div className="flex flex-col md:grid grid-cols-2 gap-2 justify-between">
+                <label htmlFor="username">User Name</label>
+                <Field
+                  id="username"
+                  name="username"
+                  type="text"
+                  className="bg-dark border rounded-lg px-2"
+                />
+              </div>
+              <small className="text-red-700 md:col-span-2 md:text-end md:pe-3">
+                <ErrorMessage name="username" component="span"></ErrorMessage>
+              </small>
+              <div className="flex flex-col md:grid grid-cols-2 gap-2 justify-between">
+                <label htmlFor="password">Password</label>
+                <Field
+                  id="password"
+                  name="password"
+                  type="password"
+                  className="bg-dark border rounded-lg px-2"
+                />
+                <small className="text-red-700 md:col-span-2 md:text-end md:pe-3">
+                  <ErrorMessage name="password" component="span"></ErrorMessage>
                 </small>
               </div>
-            </div>
-          </form>
+              <div>
+                <button type="button" className="btn mt-3">
+                  Login
+                </button>
+                <div>
+                  <small>
+                    <Link to="/signup" className="text-primary">
+                      Click here
+                    </Link>{" "}
+                    to create a new account
+                  </small>
+                </div>
+              </div>
+            </Form>
+          </Formik>
         </section>
       </div>
     </>
--- src/components/signupform/signupform.tsx
+++ src/components/signupform/signupform.tsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export default function SignUpForm() {
+  function handleSubmit(values: object) {
+    console.log(values);
+  }
   return (
     <>
       <div className="text-light text-center h-[88dvh]">
@@ -15,64 +20,105 @@ export default function SignUpForm() {
           className="flex justify-center items-start mt-5 bg-[url('/banner5.png')] md:bg-[url('/banner4.png')] h-full"
         >
           <div id="shader"></div>
-          <form
-            id="content"
-            action=""
-            className="rounded-md flex mt-36 flex-col gap-3"
+          <Formik
+            initialValues={{
+              fullname: "",
+              email: "",
+              username: "",
+              password: "",
+            }}
+            onSubmit={(values) => handleSubmit(values)}
+            validationSchema={Yup.object({
+              fullname: Yup.string().required("Please enter your name"),
+              email: Yup.string()
+                .required("Please enter your email id")
+                .email("Please enter a valid Email ID"),
+              username: Yup.string()
+                .required("Please enter a user name")
+                .matches(/^[a-z]\D+/, "Please start with a lowercase letter")
+                .min(6, "User name should be more than 6 letters")
+                .max(15, "User name should be less than 15 letters"),
+              password: Yup.string()
+                .required("Please enter a password")
+                .min(6, "Password should be more than 6 letters")
+                .max(20, "Password should be less than 20 letters")
+                .matches(
+                  /(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])\w{6,20}/,
+                  "Password should have at least one capital letter, one small letter and one number"
+                ),
+            })}
           >
-            <div id="shader"></div>
-            <div className="text-lightred">Sign Up for a new Account</div>
-            <div className="flex flex-col md:flex-row gap-2 justify-between">
-              <label htmlFor="name">Name</label>
-              <input
-                id="name"
-                name="name"
-                type="text"
-                className="bg-dark border rounded-lg px-2"
-              />
-            </div>
-            <div className="flex flex-col md:flex-row gap-2 justify-between">
-              <label htmlFor="email">Email ID</label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                className="bg-dark border rounded-lg px-2"
-              />
-            </div>
-            <div className="flex flex-col md:flex-row gap-2 justify-between">
-              <label htmlFor="username">User Name</label>
-              <input
-                id="username"
-                name="username"
-                type="text"
-                className="bg-dark border rounded-lg px-2"
-              />
-            </div>
-            <div className="flex flex-col md:flex-row gap-2 justify-between">
-              <label htmlFor="password">Password</label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                className="bg-dark border rounded-lg px-2"
-              />
-            </div>
-            <div>
-              <button type="button" className="btn mt-3">
-                Sign Up
-              </button>
-            </div>
-            <div>
-              <small>
-                Already have an account?{" "}
-                <Link to="/login" className="text-primary">
-                  Click here
-                </Link>{" "}
-                to login
-              </small>
-            </div>
-          </form>
+            <Form
+              id="content"
+              action=""
+              className="rounded-md flex mt-36 flex-col gap-3"
+            >
+              <div id="shader"></div>
+              <div className="text-lightred">Sign Up for a new Account</div>
+              <div className="flex flex-col md:grid grid-cols-2 gap-2 justify-between">
+                <label htmlFor="name">Name</label>
+                <Field
+                  id="fullname"
+                  name="fullname"
+                  type="text"
+                  className="bg-dark border rounded-lg px-2"
+                />
+                <small className="text-red-700 md:col-span-2 md:text-end md:pe-3">
+                  <ErrorMessage name="fullname" component="span"></ErrorMessage>
+                </small>
+              </div>
+              <div className="flex flex-col md:grid grid-cols-2 gap-2 justify-between">
+                <label htmlFor="email">Email ID</label>
+                <Field
+                  id="email"
+                  name="email"
+                  type="email"
+                  className="bg-dark border rounded-lg px-2"
+                />
+                <small className="text-red-700 md:col-span-2 md:text-end md:pe-3">
+                  <ErrorMessage name="email" component="span"></ErrorMessage>
+                </small>
+              </div>
+              <div className="flex flex-col md:grid grid-cols-2 gap-2 justify-between">
+                <label htmlFor="username">User Name</label>
+                <Field
+                  id="username"
+                  name="username"
+                  type="text"
+                  className="bg-dark border rounded-lg px-2"
+                />
+                <small className="text-red-700 md:col-span-2 md:text-end md:pe-3">
+                  <ErrorMessage name="username" component="span"></ErrorMessage>
+                </small>
+              </div>
+              <div className="flex flex-col md:grid grid-cols-2 gap-2 justify-between">
+                <label htmlFor="password">Password</label>
+                <Field
+                  id="password"
+                  name="password"
+                  type="password"
+                  className="bg-dark border rounded-lg px-2"
+                />
+                <small className="text-red-700 md:col-span-2 md:text-end md:pe-3">
+                  <ErrorMessage name="password" component="div"></ErrorMessage>
+                </small>
+              </div>
+              <div>
+                <button type="submit" className="btn mt-3">
+                  Sign Up
+                </button>
+              </div>
+              <div>
+                <small>
+                  Already have an account?{" "}
+                  <Link to="/login" className="text-primary">
+                    Click here
+                  </Link>{" "}
+                  to login
+                </small>
+              </div>
+            </Form>
+          </Formik>
         </section>
       </div>
     </>
