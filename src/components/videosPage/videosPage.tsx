8f8779d10ef805ac2c5e993e3b46a92261bec59e
Aadi <adithyamanikumar@proton.me>
2024-06-08 22:37:14 +0530

    Removed variables not being used because typescript was screaming at me

--- src/components/videosPage/videosPage.tsx
+++ src/components/videosPage/videosPage.tsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 export default function VideosPage() {
   const { categoryName } = useContext(Context);
   const [videosList, setVideosList] = useState([]);
-  const [cookie, setCookie, removeCookie] = useCookies(["username"]);
+  const [cookie, ,] = useCookies(["username"]);
   const navigate = useNavigate();
   interface Video {
     id: number;
