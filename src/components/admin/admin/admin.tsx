a458f8058bfea4708ddbf4c5034d9c89b25e6a77
Aadi <adithyamanikumar@proton.me>
2024-06-08 22:59:20 +0530

    Removed the 4 column layout when in large screen mode. So, max would not be a three col layout

--- src/components/admin/admin/admin.tsx
+++ src/components/admin/admin/admin.tsx
@@ -176,7 +176,7 @@ export default function Admin() {
           </div>
         </section>
         <section>
-          <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
             {videos}
           </div>
         </section>
