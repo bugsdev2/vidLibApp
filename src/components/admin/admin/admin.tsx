6fb37a1ce268f62b7f0d481e07f208d3600dd242
Aadi <adithyamanikumar@proton.me>
2024-08-06 00:17:50 +0530

    Trying to solve the bug in production code by adding a check

--- src/components/admin/admin/admin.tsx
+++ src/components/admin/admin/admin.tsx
@@ -103,48 +103,51 @@ export default function Admin() {
       });
   }
 
-  const videos = videosList.map((video: Video) => {
-    let description: string | null = null;
+  const videos =
+    videosList.length === 0
+      ? null
+      : videosList.map((video: Video) => {
+          let description: string | null = null;
 
-    if (video.description.length > 100) {
-      description = video.description.slice(0, 350);
-      description += "...";
-    }
-    let directorName: string | null = null;
-    categories.map((category: Category) => {
-      if (category.category === video.category) {
-        directorName = category.name;
-      }
-    });
-    return (
-      <div className="flex flex-col bg-[rgba(355,355,355,0.1)] hover:bg-[rgba(355,355,355,0.13)] rounded-md mb-4 p-4">
-        <div
-          key={video.id}
-          onClick={() => handleDeleteVideo(video)}
-          className="bi bi-x text-sm btn-outline border-none p-0 w-5 text-center place-self-end cursor-pointer rounded-full"
-        ></div>
-        <div className="flex flex-col  justify-stretch">
-          <div className="text-2xl text-center font-bold uppercase tracking-widest text-primary">
-            {video.title}
-          </div>
-          <div className="mb-3 text-center">A film by {directorName}</div>
-          <iframe
-            className="text-center border"
-            // width="250"
-            // height="115"
-            src={`https://www.youtube.com/embed/${video.videoCode}`}
-            title={`${video.title}`}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div title={video.description} className="mt-4 text-sm">
-          {description}
-        </div>
-      </div>
-    );
-  });
+          if (video.description.length > 100) {
+            description = video.description.slice(0, 350);
+            description += "...";
+          }
+          let directorName: string | null = null;
+          categories.map((category: Category) => {
+            if (category.category === video.category) {
+              directorName = category.name;
+            }
+          });
+          return (
+            <div className="flex flex-col bg-[rgba(355,355,355,0.1)] hover:bg-[rgba(355,355,355,0.13)] rounded-md mb-4 p-4">
+              <div
+                key={video.id}
+                onClick={() => handleDeleteVideo(video)}
+                className="bi bi-x text-sm btn-outline border-none p-0 w-5 text-center place-self-end cursor-pointer rounded-full"
+              ></div>
+              <div className="flex flex-col  justify-stretch">
+                <div className="text-2xl text-center font-bold uppercase tracking-widest text-primary">
+                  {video.title}
+                </div>
+                <div className="mb-3 text-center">A film by {directorName}</div>
+                <iframe
+                  className="text-center border"
+                  // width="250"
+                  // height="115"
+                  src={`https://www.youtube.com/embed/${video.videoCode}`}
+                  title={`${video.title}`}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                ></iframe>
+              </div>
+              <div title={video.description} className="mt-4 text-sm">
+                {description}
+              </div>
+            </div>
+          );
+        });
 
   return (
     <>
