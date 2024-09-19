ad28c076cd11fe9e988bdf36bc4b63bb637358d6
Aadi <adithyamanikumar@proton.me>
2024-09-19 22:10:48 +0530

    Previous commit didn't work. Tweaked code to check if it is working now...

--- src/components/admin/admin/admin.tsx
+++ src/components/admin/admin/admin.tsx
@@ -23,7 +23,7 @@ export default function Admin() {
   const navigate = useNavigate();
   const [cookie, ,] = useCookies(["username"]);
   const [categories, setCategories] = useState([]);
-  const [videosList, setVideosList] = useState<Video[]>([]);
+  const [videosList, setVideosList] = useState<Video[]>();
   const [modalDisplay, setModalDisplay] = useState("hidden");
   const [newCategoryDisplay, setNewCategoryDisplay] = useState("hidden");
   const categoryFormRef = useRef<any>(null);
@@ -98,53 +98,50 @@ export default function Admin() {
       });
   }
 
-  const videos =
-    videosList.length === 0
-      ? null
-      : videosList?.map((video: Video) => {
-          let description: string | null = null;
-          if (video.description.length > 100) {
-            description = video.description.slice(0, 350);
-            description += "...";
-          }
-          let directorName: string | null = null;
-          categories?.map((category: Category) => {
-            if (category.category === video.category) {
-              directorName = category.name;
-            }
-          });
-          return (
-            <div
-              key={video.id}
-              className="flex flex-col bg-[rgba(355,355,355,0.1)] hover:bg-[rgba(355,355,355,0.13)] rounded-md mb-4 p-4"
-            >
-              <div
-                onClick={() => handleDeleteVideo(video)}
-                className="bi bi-x text-sm btn-outline border-none p-0 w-5 text-center place-self-end cursor-pointer rounded-full"
-              ></div>
-              <div className="flex flex-col  justify-stretch">
-                <div className="text-2xl text-center font-bold uppercase tracking-widest text-primary">
-                  {video.title}
-                </div>
-                <div className="mb-3 text-center">A film by {directorName}</div>
-                <iframe
-                  loading="lazy"
-                  className="text-center border"
-                  // width="250"
-                  // height="115"
-                  src={`https://www.youtube.com/embed/${video.videoCode}`}
-                  title={`${video.title}`}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  referrerPolicy="strict-origin-when-cross-origin"
-                  allowFullScreen
-                ></iframe>
-              </div>
-              <div title={video.description} className="mt-4 text-sm">
-                {description}
-              </div>
-            </div>
-          );
-        });
+  const videos = videosList?.map((video: Video) => {
+    let description: string | null = null;
+    if (video.description.length > 100) {
+      description = video.description.slice(0, 350);
+      description += "...";
+    }
+    let directorName: string | null = null;
+    categories?.map((category: Category) => {
+      if (category.category === video.category) {
+        directorName = category.name;
+      }
+    });
+    return (
+      <div
+        key={video.id}
+        className="flex flex-col bg-[rgba(355,355,355,0.1)] hover:bg-[rgba(355,355,355,0.13)] rounded-md mb-4 p-4"
+      >
+        <div
+          onClick={() => handleDeleteVideo(video)}
+          className="bi bi-x text-sm btn-outline border-none p-0 w-5 text-center place-self-end cursor-pointer rounded-full"
+        ></div>
+        <div className="flex flex-col  justify-stretch">
+          <div className="text-2xl text-center font-bold uppercase tracking-widest text-primary">
+            {video.title}
+          </div>
+          <div className="mb-3 text-center">A film by {directorName}</div>
+          <iframe
+            loading="lazy"
+            className="text-center border"
+            // width="250"
+            // height="115"
+            src={`https://www.youtube.com/embed/${video.videoCode}`}
+            title={`${video.title}`}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div title={video.description} className="mt-4 text-sm">
+          {description}
+        </div>
+      </div>
+    );
+  });
 
   return (
     <>
