baf7f33a8fbf7bfd89b4b8485d604bd83e4a95a1
Aadi <adithyamanikumar@proton.me>
2024-09-19 22:49:11 +0530

    Added videosList as dependency so that it rerenders when I delete it

--- src/components/admin/admin/admin.tsx
+++ src/components/admin/admin/admin.tsx
@@ -46,7 +46,7 @@ export default function Admin() {
       .then((res) => {
         setVideosList(res.data);
       });
-  }, []);
+  }, [videosList]);
 
   function handleVideoSubmit(values: {}, resetForm: any) {
     console.log(values);
